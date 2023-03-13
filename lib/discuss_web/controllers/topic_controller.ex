13eb5c1383b7d65393ee4b0ae795fb89d18bc73c
Ivan Stoev <ivan.zstoevgmail.com>
2023-03-13 23:18:59 +0000

    hook to repo.create

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -2,13 +2,18 @@ defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
   alias Discuss.Topics.Topic
+  alias Discuss.Topics
 
-  def new(conn, params) do
+  def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
     render(conn, :new, changeset: changeset)
   end
 
   def create(conn, %{"topic" => topic} = params) do
+    case Topics.create_topic(topic) do
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} -> IO.inspect(changeset)
+    end
   end
 end
