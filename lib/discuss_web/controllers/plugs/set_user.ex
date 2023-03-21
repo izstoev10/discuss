219b5aab3f84443e607789f03851b66ab38674bf
Ivan Stoev <ivan.zstoevgmail.com>
2023-03-21 20:09:03 +0000

    latest

---
+++ lib/discuss_web/controllers/plugs/set_user.ex
@@ -0,0 +1,13 @@
+defmodule Discuss.Plugs.SetUser do
+  import Plug.Conn
+  import Phoenix.Controller
+
+  alias Discuss.Users.User
+  alias Discuss.Repo
+
+  def init(_params) do
+  end
+
+  def call(conn, _params) do
+  end
+end
