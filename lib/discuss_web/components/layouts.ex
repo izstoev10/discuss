95fd35f50a26c60e88f7afac75aa304d18411c75
Ivan Stoev <ivan.zstoevgmail.com>
2023-03-22 20:26:15 +0000

    add logout/login links

--- lib/discuss_web/components/layouts.ex
+++ lib/discuss_web/components/layouts.ex
@@ -1,5 +1,7 @@
 defmodule DiscussWeb.Layouts do
   use DiscussWeb, :html
+  import Phoenix.HTML.Link
+  import Phoenix.HTML.Form
 
   embed_templates "layouts/*"
 end
--- lib/discuss_web/controllers/plugs/set_user.ex
+++ lib/discuss_web/controllers/plugs/set_user.ex
@@ -4,10 +4,20 @@ defmodule Discuss.Plugs.SetUser do
 
   alias Discuss.Users.User
   alias Discuss.Repo
+  alias DiscussWeb.Router.Helpers
 
   def init(_params) do
   end
 
   def call(conn, _params) do
+    user_id = get_session(conn, :user_id)
+
+    cond do
+      user = user_id && Repo.get(User, user_id) ->
+        assign(conn, :user, user)
+
+      true ->
+        assign(conn, :user, nil)
+    end
   end
 end
